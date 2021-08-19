35ace108b249f2276499bab1f4d4a04fa036d338
Gray Programmerz <hackrefisher@gmail.com>
2021-08-19 18:14:27 +0500

    Update label.h

--- include/ui/label.h
+++ include/ui/label.h
@@ -3,74 +3,74 @@ typedef struct
 {
 	LPCWSTR text;
 	HWND parent, me;
-	int x, y, width, height, id;
+	int x, y, width, height,ID;
 	BOOLEAN visible;
 
 } label;
 
 label Label()
 {
-	label lbl;
-	lbl.text = L"";
-	lbl.x = 0;
-	lbl.y = 0;
-	lbl.width = 10;
-	lbl.height = 10;
-	lbl.visible = 1;
-	return lbl;
+	label btn;
+	btn.text = L"";
+	btn.x = 0;
+	btn.y = 0;
+	btn.width = 10;
+	btn.height = 10;
+	btn.visible = 1;
+	return btn;
+}
+
+
+BOOL label_setVisible(label *l, BOOL VISIBLE)
+{
+	l->visible = VISIBLE;
+	return ShowWindow(l->me, (VISIBLE ? SW_SHOW : SW_HIDE));
 }
 
 BOOL label_draw(label *l)
 {
-	l->me = CreateWindowExW(0, L"Static", l->text, (l->visible ? WS_VISIBLE | WS_CHILD : WS_CHILD), l->x, l->y, l->width, l->height, l->parent, (HMENU)l->id, NULL, NULL);
+	l->me = CreateWindowExW(0, L"label", l->text, (l->visible ? WS_VISIBLE | WS_CHILD : WS_CHILD), l->x, l->y, l->width, l->height, l->parent, (HMENU)l->ID, NULL, NULL);
 	return l->me != NULL;
 }
 
 BOOL label_setID(label *l, int ID)
 {
-	BOOL result = SetMenu(l->me, (HMENU)ID);
-	l->id = ID;
-	return result;
+	l->ID = ID;
+	return SetMenu(l->me, (HMENU)ID);
 }
 
 BOOL label_setParent(label *l, HWND PARENT)
 {
-	BOOL result = SetParent(l->me, PARENT) != NULL; 
 	l->parent = PARENT;
-	return result;
+	return SetParent(l->me, PARENT) != NULL;
 }
 
 BOOL label_setWidth(label *l, int WIDTH)
 {
-	BOOL result = SetWindowPos(l->me, 0, 0, 0, WIDTH, l->height, SWP_NOMOVE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 	l->width = WIDTH;
-	return result;
+	return SetWindowPos(l->me, 0, 0, 0, WIDTH, l->height, SWP_NOMOVE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 }
 
 BOOL label_setHeight(label *l, int HEIGHT)
 {
-	BOOL result = SetWindowPos(l->me, 0, 0, 0, l->width, HEIGHT, SWP_NOMOVE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 	l->height = HEIGHT;
-	return result;
+	return SetWindowPos(l->me, 0, 0, 0, l->width, HEIGHT, SWP_NOMOVE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 }
 
 BOOL label_setX(label *l, int X)
 {
-	BOOL result = SetWindowPos(l->me, 0, X, l->y, 0, 0, SWP_NOSIZE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 	l->x = X;
-	return result;
+	return SetWindowPos(l->me, 0, X, l->y, 0, 0, SWP_NOSIZE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 }
 
 BOOL label_setY(label *l, int Y)
 {
-	BOOL result = SetWindowPos(l->me, 0, l->x, Y, 0, 0, SWP_NOSIZE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 	l->y = Y;
-	return result;
+	return SetWindowPos(l->me, 0, l->x, Y, 0, 0, SWP_NOSIZE | (l->visible ? SWP_SHOWWINDOW : SWP_HIDEWINDOW));
 }
 
 BOOL label_setText(label *l, LPCWSTR TEXT)
 {
-	BOOL result = SetWindowTextW(l->me, TEXT);
 	l->text = TEXT;
-	return result;
+	return SetWindowTextW(l->me, TEXT);
 }
